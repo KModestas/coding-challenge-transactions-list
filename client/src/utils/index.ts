30a40be684c636eab7a4436720c9c56e37808561
Modestas <modestask97@gmail.com>
2024-03-04 11:01:08 +0000

    fix comment typos

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -1,6 +1,6 @@
 import { ethers } from 'ethers';
 
-// #7 - Add utility functions to convert values to humand reasable formats. Centralise these functions inside a single place so that they can easily be used throughout the app.
+// #7 - Add utility functions to convert values to human readable formats. Centralise these functions inside a single place so that they can easily be used throughout the app.
 
 export const convertWeiToEth = (wei = '') => {
   return ethers.formatEther(wei);
